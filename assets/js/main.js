6f82f8960ee4e4e030f2801a57bb091328d2048c
ClaudioSUPERCODE <123948180+ClaudioSUPERCODE@users.noreply.github.com>
2023-02-17 16:39:12 +0100

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,133 @@
+// -----LEV-1_1--console.log---------//
+
+// let a = 12
+// console.log (a)
+
+// let b = a*3
+// console.log (b)
+
+// document.write (b)
+
+// console.log ("Hello World!")
+
+// console.log ("Anton")
+
+// const firstName = ("Anton")
+// console.log (firstName)
+
+// let age = (28)
+// console.log (age)
+
+// let job = "Trainer"
+// console.log (job)
+
+// let married = true
+// console.log (married)
+
+// console.log (firstName+" ist "+age+" Jahre alt, von Beruf "+job+" und ist verheiratet: "+married)
+
+// --------LEV-1_4-popup_boxes-----------//
+
+// window.alert ("Hallo Welt")
+// window.prompt ("bitte geben Sie Ihren Namen ein.")
+// window.prompt ("Bitte geben Sie Ihren Namen ein", "Cancel")
+// window.prompt ("Stimmen Sie meiner Meinung zu?")
+
+// let alter = window.prompt ("Gib mir bitte dein Alter:")
+// console.log (alter)
+
+// let b = 5
+// let a = b*5-3
+// window.alert (a)
+
+// let mann = "Robert Wadlow ist der größte Mann der Welt : "
+// let groesse = "2.72m"
+// window.confirm (mann+groesse)
+
+// ----------LEV-1_5-variables-------------//
+
+// const carName = "BMW"
+// console.log (carName)
+
+// let x = 150
+// console.log (x)
+// let y = 50
+// console.log (y)
+
+// z = x+y
+// console.log (z)
+
+// const firstName = "John"
+// const lastName = "Doe"
+// let age = 35
+// console.log (firstName+lastName+age)
+
+// -------------LEV-1_6-let_const-------------//
+
+// let x = 34
+// x= 67
+// console.log (x)
+
+// const y = 34
+// y = 67
+// console.log (y)
+
+// ======DER UNTERSCHIED IST. KONSTANTEN LASSEN SICH NICHT ÄNDERN========//
+
+//----------LEV-1_7-coding_conventions---------//
+
+// let helloWorld = "Hello World"
+// const pi = 3.14
+// const burjDubai = "828m"
+// const fullName = "Jan Schmidt"
+// const eifelTower = "324m"
+// const camelCase = "camelCase"
+// let kursStatus = true
+
+// console.log (helloWorld+pi+burjDubai+fullName+eifelTower+camelCase+kursStatus)
+
+// -----------LEV-1_12-arithmetische_operatoren-----------//
+
+// let score = 5+5*10
+// console.log (score)
+
+// let score = (5+5)*10
+// console.log (score)
+
+// let score = 0
+// score = score + 10
+// console.log ("Ergebnis: "+score)
+
+// score += 10
+// console.log ("ergebnis: "+score)
+
+// let zahl = 1
+// zahl = zahl+1
+// console.log (zahl+1)
+
+// zahl += 1
+// console.log (zahl)
+
+// zahl++
+// console.log ('increment:' +zahl)
+
+// zahl--
+// console.log ('dekrement:' +zahl)
+
+// -----------------LEV-1_13-arithmetische_wiederholung---------//
+
+let addition_operator = 1+1
+console.log ("addition: " + addition_operator)
+
+let subtraktion_operator = 2-1
+console.log ("subtraktion: " + subtraktion_operator)
+
+let multiplication_operator = 2*2
+console.log ("multiplication: " + multiplication_operator)
+
+let division_operator = 4/2
+console.log ("division: " + division_operator)
+
+let modulus_operator = 14%5
+console.log ("modulus: " + modulus_operator)
+
